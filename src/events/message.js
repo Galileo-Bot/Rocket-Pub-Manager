56742b64a5997b14ae73b466cdc6fa2249595a40
Ayfri <pierre.ayfri@gmail.com>
2020-11-07 17:32:09 +0100

    Added message event.

--- src/events/message.js
+++ src/events/message.js
@@ -1,3 +1,16 @@
+const {getThing} = require('advanced-command-handler');
 module.exports = async (handler, message) => {
 	if (message.author.bot || message.system) return;
+	
+	const args = message.content.slice(handler.prefixes[0].length).trim().split(/\s+/g);
+	/**
+	 * @type {Command}
+	 */
+	const command = await getThing('command', args.shift());
+	
+	if (command) {
+		if (message.guild?.id === '465918902254436362' && handler.client.hasPermission(message, 'ADMINISTRATOR')) {
+			command.run(handler, message, args);
+		}
+	}
 };
