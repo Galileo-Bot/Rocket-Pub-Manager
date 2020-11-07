1c1a5630ae3169459333583dd21100f787c5d637
Ayfri <pierre.ayfri@gmail.com>
2020-11-07 17:32:24 +0100

    Added 'addServeurInterdit' command.

--- src/commands/administrator/addServeurInterdit.js
+++ src/commands/administrator/addServeurInterdit.js
@@ -1,3 +1,11 @@
-module.exports = async(handler, message, args) => {
-	
-}
+const {Command} = require('advanced-command-handler');
+module.exports = new Command({
+	name: 'addServeurInterdit',
+	aliases: ['asi', 'addSI'],
+	usage: 'addServeurInterdit <id>',
+	userPermissions: ['ADMINISTRATOR'],
+	guildOnly: true,
+	description: 'Ajoute un serveur aux serveurs interdits.'
+}, async(handler, message, args) => {
+
+})
