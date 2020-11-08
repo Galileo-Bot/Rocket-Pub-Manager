de10265bd71f69da9a70460b1427ac8a55b9c6f7
Ayfri <pierre.ayfri@gmail.com>
2020-11-08 16:28:14 +0100

    Added utils.

---
+++ src/utils/Utils.js
@@ -0,0 +1,11 @@
+async function getGuild(handler, args) {
+	let guild;
+	await handler.client.fetchInvite(args[0])
+	             .then(invite => guild = invite.guild.vanityURLCode === args[0] ? args[0].match(/\d{17,19}/g) ? args[0] : null : invite.guild)
+	             .catch(() => guild = args[0].match(/\d{17,19}/g) ? args[0] : null);
+	return guild;
+}
+
+module.exports = {
+	getGuild
+};
