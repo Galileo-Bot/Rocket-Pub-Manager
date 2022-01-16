3cdac1565faca3a52e778dd0c85180e046e428ab
Ayfri <pierre.ayfri@gmail.com>
2022-01-16 12:58:03 +0100

    refactor: Move command to modify sanctions into separate extension.

---
+++ src/main/kotlin/extensions/ModifySanctions.kt
@@ -0,0 +1,94 @@
+package extensions
+
+import com.kotlindiscord.kord.extensions.DiscordRelayedException
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.application.slash.converters.impl.enumChoice
+import com.kotlindiscord.kord.extensions.commands.application.slash.publicSubCommand
+import com.kotlindiscord.kord.extensions.commands.converters.impl.coalescedString
+import com.kotlindiscord.kord.extensions.commands.converters.impl.duration
+import com.kotlindiscord.kord.extensions.commands.converters.impl.int
+import com.kotlindiscord.kord.extensions.commands.converters.impl.member
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
+import storage.SanctionType
+import storage.modifySanction
+import utils.ROCKET_PUB_GUILD
+
+
+enum class ModifySanctionValues(val translation: String) {
+	APPLIED_BY("Appliquée par"),
+	DURATION("Durée"),
+	REASON("Raison"),
+	TYPE("Type"),
+}
+
+class ModifySanctions : Extension() {
+	override val name = "Modify-Sanctions"
+	
+	abstract class ModifySanction : Arguments() {
+		val id by int("cas", "Le numéro de la sanction à modifier.")
+	}
+	
+	class ModifyAppliedByArguments : ModifySanction() {
+		val appliedBy by member("modérateur", "Par qui la sanction a été appliquée.", { ROCKET_PUB_GUILD }) { _, member ->
+			if (!isStaff(member)) {
+				throw DiscordRelayedException("La personne n'a pas le rôle staff et n'est donc pas modérateur, impossible de l'utiliser.")
+			}
+		}
+	}
+	
+	class ModifyDurationArguments : ModifySanction() {
+		val duration by duration("duree", "Durée de la sanction.")
+	}
+	
+	class ModifyReasonArguments : ModifySanction() {
+		val reason by coalescedString("raison", "Raison de la sanction.")
+	}
+	
+	class ModifySanctionTypeArguments : ModifySanction() {
+		val type by enumChoice<SanctionType>("type", "Type de la sanction.", "type")
+	}
+	
+	override suspend fun setup() {
+		publicSlashCommand {
+			name = "modifier"
+			description = "Permet de modifier une sanction."
+			
+			publicSubCommand(ModifySanctions::ModifyAppliedByArguments) {
+				name = "modérateur"
+				description = "Permet de modifier le modérateur de la sanction."
+				
+				action {
+					modifySanction(arguments.id, ModifySanctionValues.APPLIED_BY, arguments.appliedBy.id.asString)
+				}
+			}
+			
+			publicSubCommand(ModifySanctions::ModifyDurationArguments) {
+				name = "durée"
+				description = "Permet de modifier la durée de la sanction."
+				
+				action {
+					modifySanction(arguments.id, ModifySanctionValues.DURATION, arguments.duration.toString())
+				}
+			}
+			
+			publicSubCommand(ModifySanctions::ModifyReasonArguments) {
+				name = "raison"
+				description = "Permet de modifier la raison de la sanction."
+				
+				action {
+					modifySanction(arguments.id, ModifySanctionValues.REASON, arguments.reason)
+				}
+			}
+			
+			publicSubCommand(ModifySanctions::ModifySanctionTypeArguments) {
+				name = "type"
+				description = "Permet de modifier le type de la sanction."
+				
+				action {
+					modifySanction(arguments.id, ModifySanctionValues.TYPE, arguments.type.name)
+				}
+			}
+		}
+	}
+}
--- src/main/kotlin/extensions/Sanctions.kt
+++ src/main/kotlin/extensions/Sanctions.kt
@@ -1,12 +1,9 @@
 package extensions
 
-import com.kotlindiscord.kord.extensions.DiscordRelayedException
 import com.kotlindiscord.kord.extensions.commands.Arguments
-import com.kotlindiscord.kord.extensions.commands.application.slash.converters.impl.enumChoice
 import com.kotlindiscord.kord.extensions.commands.application.slash.converters.impl.optionalEnumChoice
 import com.kotlindiscord.kord.extensions.commands.application.slash.publicSubCommand
 import com.kotlindiscord.kord.extensions.commands.converters.impl.coalescedString
-import com.kotlindiscord.kord.extensions.commands.converters.impl.duration
 import com.kotlindiscord.kord.extensions.commands.converters.impl.int
 import com.kotlindiscord.kord.extensions.commands.converters.impl.member
 import com.kotlindiscord.kord.extensions.commands.converters.impl.user
@@ -23,23 +20,14 @@ import storage.SanctionType
 import storage.containsSanction
 import storage.getSanctionCount
 import storage.getSanctions
-import storage.modifySanction
 import storage.removeSanction
 import storage.removeSanctions
-import utils.ROCKET_PUB_GUILD
 import utils.completeEmbed
 import utils.sanctionEmbed
 
-enum class ModifySanctionValues(val translation: String) {
-	APPLIED_BY("Appliquée par"),
-	DURATION("Durée"),
-	REASON("Raison"),
-	TYPE("ID"),
-}
-
 @OptIn(KordPreview::class)
 class Sanctions : Extension() {
-	override val name: String = "Sanctions"
+	override val name = "Sanctions"
 	
 	class DeleteSanctionArguments : Arguments() {
 		val id by int("cas", "Le numéro de la sanction à supprimer.")
@@ -52,30 +40,6 @@ class Sanctions : Extension() {
 	
 	class ListSanctionsArguments : Arguments() {
 		val user by user("user", "L'utilisateur à qui afficher les sanctions.")
-	}
-	
-	abstract class ModifySanction : Arguments() {
-		val id by int("cas", "Le numéro de la sanction à modifier.")
-	}
-	
-	class ModifyAppliedByArguments : ModifySanction() {
-		val appliedBy by member("modérateur", "Par qui la sanction a été appliquée.", { ROCKET_PUB_GUILD }) { _, member ->
-			if (!isStaff(member)) {
-				throw DiscordRelayedException("La personne n'a pas le rôle staff et n'est donc pas modérateur, impossible de l'utiliser.")
-			}
-		}
-	}
-	
-	class ModifyDurationArguments : ModifySanction() {
-		val duration by duration("duree", "Durée de la sanction.")
-	}
-	
-	class ModifyReasonArguments : ModifySanction() {
-		val reason by coalescedString("raison", "Raison de la sanction.")
-	}
-	
-	class ModifySanctionTypeArguments : ModifySanction() {
-		val type by enumChoice<SanctionType>("type", "Type de la sanction.", "type")
 	}
 	
 	class WarnArguments : Arguments() {
@@ -196,47 +160,6 @@ class Sanctions : Extension() {
 							}
 						}
 					}
-				}
-			}
-		}
-		
-		publicSlashCommand {
-			name = "modifier"
-			description = "Permet de modifier une sanction."
-			
-			publicSubCommand(::ModifyAppliedByArguments) {
-				name = "modérateur"
-				description = "Permet de modifier le modérateur de la sanction."
-				
-				action {
-					modifySanction(arguments.id, ModifySanctionValues.APPLIED_BY, arguments.appliedBy.id.asString)
-				}
-			}
-			
-			publicSubCommand(::ModifyDurationArguments) {
-				name = "durée"
-				description = "Permet de modifier la durée de la sanction."
-				
-				action {
-					modifySanction(arguments.id, ModifySanctionValues.DURATION, arguments.duration.toString())
-				}
-			}
-			
-			publicSubCommand(::ModifyReasonArguments) {
-				name = "raison"
-				description = "Permet de modifier la raison de la sanction."
-				
-				action {
-					modifySanction(arguments.id, ModifySanctionValues.REASON, arguments.reason)
-				}
-			}
-			
-			publicSubCommand(::ModifySanctionTypeArguments) {
-				name = "type"
-				description = "Permet de modifier le type de la sanction."
-				
-				action {
-					modifySanction(arguments.id, ModifySanctionValues.TYPE, arguments.type.name)
 				}
 			}
 		}
