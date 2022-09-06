41b7f0606998273df0eb91ef9954dc42a126cd8b
Ayfri <pierre.ayfri@gmail.com>
2022-09-06 10:05:50 +0200

    feat: Document channels, add logs verif channel.

--- src/main/kotlin/utils/RocketPubUtils.kt
+++ src/main/kotlin/utils/RocketPubUtils.kt
@@ -14,13 +14,29 @@ const val DISCORD_INVITE_LINK_REGEX = "(?:https?:\\/\\/)?(?:\\w+\\.)?discord(?:(
 const val AD_CATEGORY_CHANNEL_EMOTE = "🔗"
 const val AD_CHANNEL_EMOTE = "<:validate:525405975289659402>"
 
+/** Salon où les erreurs du bot sont envoyées */
 val ERROR_CHANNEL = Snowflake("864756196539105290")
+
+/** Serveur du bot */
 val ROCKET_PUB_GUILD = Snowflake("465918902254436362")
+
+/** Serveur staff */
 val ROCKET_PUB_GUILD_STAFF = Snowflake("770763755265064980")
-val SANCTION_LOGGER_CHANNEL = Snowflake(configuration["AYFRI_ROCKETMANAGER_CHANNEL_SANCTION_ID"])
-val SANCTION_VERIF_CHANNEL = Snowflake(configuration["AYFRI_ROCKETMANAGER_CHANNEL_VERIF_ID"])
+
+/** Salon des logs pour les sanctions */
+val SANCTION_LOGS_CHANNEL = Snowflake(configuration["AYFRI_ROCKETMANAGER_CHANNEL_SANCTION_ID"])
+
+/** Rôle de staff */
 val STAFF_ROLE = Snowflake("494521544618278934")
+
+/** Émoji de validation */
 val VALID_EMOJI = Snowflake("525405975289659402")
+
+/** Salon des vérifications */
+val VERIF_CHANNEL = Snowflake(configuration["AYFRI_ROCKETMANAGER_CHANNEL_VERIF_ID"])
+
+/** Salon des logs pour les vérifications */
+val VERIF_LOGS_CHANNEL = Snowflake(configuration["AYFRI_ROCKETMANAGER_CHANNEL_VERIF_LOGS_ID"])
 
 fun ChannelBehavior.isAdChannel() = this is TextChannel && topic?.contains(AD_CHANNEL_EMOTE) == true
 fun ChannelBehavior.isCategoryChannel() = this is TextChannel && topic?.contains(AD_CATEGORY_CHANNEL_EMOTE) == true
@@ -39,6 +55,7 @@ suspend fun <T : Event> CheckContext<T>.isInAdCategoryChannel() {
 	failIfNot("Channel isn't an ad category channel.") { channel.fetchChannel().isCategoryChannel() }
 }
 
-suspend fun Kord.getVerifChannel() = getChannelOf<TextChannel>(SANCTION_VERIF_CHANNEL, EntitySupplyStrategy.cacheWithCachingRestFallback)!!
-suspend fun Kord.getLogSanctionsChannel() = getChannelOf<TextChannel>(SANCTION_LOGGER_CHANNEL, EntitySupplyStrategy.cacheWithCachingRestFallback)!!
+suspend fun Kord.getVerifChannel() = getChannelOf<TextChannel>(VERIF_CHANNEL, EntitySupplyStrategy.cacheWithCachingRestFallback)!!
+suspend fun Kord.getLogSanctionsChannel() = getChannelOf<TextChannel>(SANCTION_LOGS_CHANNEL, EntitySupplyStrategy.cacheWithCachingRestFallback)!!
+suspend fun Kord.getVerifLogsChannel() = getChannelOf<TextChannel>(VERIF_LOGS_CHANNEL, EntitySupplyStrategy.cacheWithCachingRestFallback)!!
 suspend fun Kord.getRocketPubGuild() = getGuild(ROCKET_PUB_GUILD, EntitySupplyStrategy.cacheWithCachingRestFallback)!!
