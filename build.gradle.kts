afd7e346630cc9bd641dd86a365a22521ef7c2a2
Ayfri <pierre.ayfri@gmail.com>
2025-01-26 04:09:03 +0100

    refactor(build): Update `build.gradle.kts` to include version in manifest and remove archive classifier.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,66 +2,72 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    alias(libs.plugins.kotlin)
-    alias(libs.plugins.serialization)
-    alias(libs.plugins.shadow)
-    application
+	alias(libs.plugins.kotlin)
+	alias(libs.plugins.serialization)
+	alias(libs.plugins.shadow)
+	application
 }
 
 group = "fr.ayfri"
 version = "1.0"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 
-    maven {
-        name = "Sonatype Releases"
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
+	maven {
+		name = "Sonatype Releases"
+		url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+	}
 
-    maven {
-        name = "Sonatype Snapshots"
-        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
-    }
+	maven {
+		name = "Sonatype Snapshots"
+		url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+	}
 
 
-    maven {
-        name = "Kotlin Discord"
-        url = uri("https://repo.kord.dev/snapshots")
-    }
+	maven {
+		name = "Kotlin Discord"
+		url = uri("https://repo.kord.dev/snapshots")
+	}
 }
 
 dependencies {
-    implementation(libs.kord.extensions)
-    implementation(libs.kord.unsafe)
-    implementation(libs.kord.base)
-    implementation(libs.dotenv)
+	implementation(libs.kord.extensions)
+	implementation(libs.kord.unsafe)
+	implementation(libs.kord.base)
+	implementation(libs.dotenv)
 
-    implementation(libs.groovy)
-    implementation(libs.logback)
-    implementation(libs.logging)
+	implementation(libs.groovy)
+	implementation(libs.logback)
+	implementation(libs.logging)
 
-    implementation(libs.serialization)
-    implementation(libs.connector)
-    implementation(libs.datetime)
+	implementation(libs.serialization)
+	implementation(libs.connector)
+	implementation(libs.datetime)
 }
 
 kotlin {
-    jvmToolchain(21)
+	jvmToolchain(21)
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "21"
-    kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
+	kotlinOptions.jvmTarget = "21"
+	kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
 
 tasks.withType<ShadowJar> {
-    manifest.attributes.apply {
-        put("Implementation-Title", "Rocket Manager")
-        put("Main-Class", "MainKT")
-    }
+	manifest {
+		attributes(
+			"Implementation-Title" to "Rocket Manager",
+			"Main-Class" to "fr.ayfri.rocketmanager.MainKt",
+			"Implementation-Version" to project.version
+		)
+	}
+
+	mergeServiceFiles()
+	archiveClassifier.set("")
 }
 
 application {
-    mainClass.set("MainKt")
+	mainClass.set("MainKt")
 }
