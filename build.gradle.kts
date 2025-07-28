ba12b6f4b1735865233a3c07af8df14c9ad3ce18
Ayfri <pierre.ayfri@gmail.com>
2025-07-28 23:53:08 +0200

    refactor(build): Simplify build configuration by removing unused dependencies and updating JVM target.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import dev.kordex.gradle.plugins.kordex.DataCollection
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
 	alias(libs.plugins.kotlin)
@@ -8,7 +7,6 @@ plugins {
 	alias(libs.plugins.shadow)
 	alias(libs.plugins.ksp)
 	alias(libs.plugins.kordex)
-	application
 }
 
 group = "fr.ayfri"
@@ -18,19 +16,19 @@ dependencies {
 	implementation(libs.dotenv)
 
 	implementation(libs.logback)
-	implementation(libs.logging)
 
-	implementation(libs.serialization)
 	implementation(libs.connector)
 	implementation(libs.datetime)
 }
 
 kordEx {
 	kordExVersion = libs.versions.kord.extensions.get()
+	jvmTarget = 21
 
 	bot {
 		dataCollection(DataCollection.None)
-		mainClass = "fr.ayfri.rocketmanager.MainKt"
+		mainClass = "MainKt"
+		voice = false
 	}
 
 	i18n {
@@ -43,7 +41,6 @@ kotlin {
 	jvmToolchain(21)
 
 	compilerOptions {
-		jvmTarget = JvmTarget.JVM_21
 		freeCompilerArgs = listOf("-Xcontext-receivers", "-opt-in=kotlin.time.ExperimentalTime")
 	}
 }
@@ -59,8 +56,4 @@ tasks.withType<ShadowJar> {
 
 	mergeServiceFiles()
 	archiveClassifier.set("")
-}
-
-application {
-	mainClass.set("MainKt")
 }
